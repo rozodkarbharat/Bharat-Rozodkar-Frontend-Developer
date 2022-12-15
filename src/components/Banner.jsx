ec2a09d92b98db8b27e6d921c9b432cbd1ce4748
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-12-15 17:00:39 +0530

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import "../css/banner.css"
 const Banner = () => {
   return (
-    <div className='banner-main'>
+    <div className="banner-main">
       <h1>Banner</h1>
       {/* <img src='rocket1.webp' alt=''/> */}
+      <p className="banner-text">
+        STARSHIP SELECTED BY NASA TO SUPPORT SUSTAINED LUNAR EXPLORATION
+      </p>
+      <div>
+        <a
+          className="banner-btn"
+          href="https://www.spacex.com/updates/#sustained-lunar-exploration"
+        >
+          Learn More
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Banner
