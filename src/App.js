8a4da0ed28a261c67c2f1ca7b0d255cfac51d8a1
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-12-15 16:09:03 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Banner from './components/Banner';
+import SearchBar from './components/SearchBar';
+import Grid from './components/Grid';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <SearchBar />
+      <Grid/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/banner.css"
+const Banner = () => {
+  return (
+    <div className='banner-main'>
+      <h1>Banner</h1>
+      {/* <img src='rocket1.webp' alt=''/> */}
+    </div>
+  )
+}
+
+export default Banner
